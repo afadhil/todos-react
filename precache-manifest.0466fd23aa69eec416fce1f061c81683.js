bd5fbea684f7dbc33505a3c18973e322dbc81108
Fadhil <agungfadhil23@gmail.com>
2019-10-14 02:26:46 +0700

    first commit

---
+++ precache-manifest.0466fd23aa69eec416fce1f061c81683.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c04952e94aaaccdc7eb8",
+    "url": "/static/js/main.c04952e9.chunk.js"
+  },
+  {
+    "revision": "1e692a086fa3f1ebeaa6",
+    "url": "/static/js/1.1e692a08.chunk.js"
+  },
+  {
+    "revision": "c04952e94aaaccdc7eb8",
+    "url": "/static/css/main.c9ff9e55.chunk.css"
+  },
+  {
+    "revision": "cc9e257692a4de117688fd669867c2b0",
+    "url": "/index.html"
+  }
+];
